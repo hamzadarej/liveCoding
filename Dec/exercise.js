f1af1af9d52dc091e8c53ff0d56183461f1ad42c
hamza <darejhamza@gmail.com>
2021-01-07 15:48:07 +0100

    Deccommit

---
+++ Dec/exercise.js
@@ -0,0 +1,18 @@
+var x = "house";
+console.log(x);
+
+console.log(x[2])
+console.log(x[6])
+var name = "hamza";
+let age = 27;
+let year = 2020;
+var y = " is ";
+var x = " in ";
+console.log(name+y+age+x+year);
+var c = " is Married";
+console.log(name+y+age+x+year+c);
+var programmingLanguage= "javaScript";
+var isFun = "true";
+console.log(programmingLanguage[0])+console.log(programmingLanguage[4]);
+console.log(programmingLanguage. length)
+
---
+++ Dec/index.js
@@ -0,0 +1,59 @@
+console.log('hamza');
+console.log("i love leipzig");
+console.log(1+3);
+console.log(12==10);   
+console.log("hamza");
+let x = 7;
+let add = 'leipzig ,04299'
+console.log(x);
+console.log(x);
+let p =10 ;
+console.log(p);
+console.log(x*p);
+let sum = 22 + 2 ;
+console.log (sum*x-p)
+var name = "hamza"
+console.log(name)
+let age ;
+console.log(age);
+age = 27
+console.log(age);
+//ternary condition
+
+let person =19;
+person > 18 ?  console.log ("go party") : console.log ("stay home");
+
+ let drive = 40 
+ drive = 39 ? console.log ("can drive yeah") : console.log ("nope sorry");
+
+let cash = 5000
+cash >= 6000 ? console.log("can buy it "): console.log ("take a credit")
+
+
+ 
+ // comparision operators (===,!==,>=,<=,>,<)
+ let num = 1;
+ let num1 = 1;
+ console.log(num == num1)
+  
+ let userName = "hamzadarej";
+
+ console.log (userName)
+ console.log(userName. length)
+ let email = "h****@gmail.com";
+ console.log(email[5])
+
+ email [0] == "@" ? console.log("enter") : console.log("this")
+
+ 
+
+
+
+
+
+
+
+
+
+
+
