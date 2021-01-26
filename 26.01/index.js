7e99612b208ef26d9af8435b56445e7c47074bba
hamza <darejhamza@gmail.com>
2021-01-26 12:33:55 +0100

    assignments

--- 26.01/index.js
+++ 26.01/index.js
@@ -1,4 +1,4 @@
-/*const ageCounter =(age)=>{
+const ageCounter =(age)=>{
     let text;
     if (age>=18){text="you are welcome"}else{text="you are not allowed here"}
   
@@ -26,13 +26,14 @@ const numbers =(...nums)=>{
 };
 numbers(2,4,5,7,8,6)//=> 6 
 //cool functions
-(function printName(c,v){
+
+/*(function printName(c, v){
     console.log(`hi i am cool function ${c+v}`);
-})(2,5);
+})(2,5);*/
 const printName1=(a,z)=>{
     return `hi i am cool function ${a+z}`
 }
-console.log(printName1(5,4));*/
+console.log(printName1(5,4));
 // assignments 
 //1
 const twofer=(str)=>{
@@ -57,14 +58,25 @@ return arr.length;
 }
 console.log(howManyArgs("better",false,1))
 4//
-const sum=(...arrs)=>{
+const sumN=(...arrs)=>{
     count = 0;
     for(let i=0;i<arrs.length;i++){
         count += arrs[i]
     }
     return count
 }
-console.log(sum(25, 25, 20))
-console.log(sum(1, 15))
-
+console.log(sumN(25, 25, 20))
+console.log(sumN(1, 15))
+// 4
+const average=(...arr)=>{
+    result=0
+    arrN =arr.length
+    for(let i=0;i<arr.length;i++){
+        result+=arr[i]/arrN
+    }
+    return result
+}
+console.log(average(1, 2)) 
+console.log(average(1, 3, 6, 10))
+console.log(average(12, 14, 16))
     
