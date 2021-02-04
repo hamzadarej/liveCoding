a1686c7b257063464a4741ce5f6cde8d7ba6b951
hamza <darejhamza@gmail.com>
2021-02-04 16:43:59 +0100

    objects

--- 04.02/index.js
+++ 04.02/index.js
@@ -103,15 +103,53 @@ Examples:
      
      result={};
          for(let i=0;i<str.length;i++){
-         const ch =str[i]
-         if(!result[ch]){result[ch]=0}
-         result[ch]++}
+         const strI =str[i]
+         if(!result[strI]){result[strI]=0}
+         result[strI]++}
         
          return result
          }
  console.log(countCH("qqqaq"))
  console.log(countCH("abc"))
- console.log("-------------------------");
+ 
+ 
+// Zodiac sign, write a function that tells the user his/her Zodiac sign. The user should enter only his birthday like dd-mm-yy
+// for farther information check https://en.wikipedia.org/wiki/Zodiac
+// Examples:
+// zodiac(“14-02-2002”)  -> Aquarius
+// zodiac(“10-06-1984")  -> Gemini (edited) 
+
+const signs = [
+    {name: "Aries",   start: "22-03", end: "20-04"},
+    {name: "Taurus",  start: "20-04", end: "21-05"},
+    {name: "Gemini",  start: "21-05", end: "21-06"},
+    {name: "Cancer",  start: "21-06", end: "23-07"},
+    {name: "Leo",     start: "23-07", end: "23-08"},
+    {name: "Virgo",   start: "23-08", end: "23-09"},
+    {name: "Libra",   start: "23-09", end: "23-10"},
+    {name: "Scorpio", start: "23-10", end: "22-11"},
+    {name: "Sagittarius", start: "23-11", end: "22-12"},
+    {name: "Capricorn",start: "22-12", end: "20-01"},
+    {name: "Aquarius", start: "20-01", end: "19-02"},
+    {name: "Pisces",   start: "19-02", end: "21-03"},
+  ]
+  console.log("------------zzz-------------");
+  const zodiac =(str)=>{
+      
+dateCounter=parseInt(str.slice(0,2))+parseInt(str.slice(3,5))*30
+const date=signs.filter((a)=>parseInt(a.start.slice(0,2))+parseInt(a.start.slice(3,5)*30)<dateCounter&&parseInt(a.end.slice(0,2))+parseInt(a.end.slice(3,5)*30)>dateCounter)
+
+return date[0]["name"]
+}
+console.log(zodiac("10-06"))
+console.log(zodiac("14-02"))
+console.log(zodiac("27-07-2020"))
+
+ 
+
+
+  
+
 
 
  
