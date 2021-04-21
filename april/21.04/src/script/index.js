0a753af0cf01545adb19fa3ed95b9fc5c4ef8582
hamza <darejhamza@gmail.com>
2021-04-21 11:23:04 +0200

    random shapes

--- april/21.04/src/script/index.js
+++ april/21.04/src/script/index.js
@@ -6,21 +6,12 @@ function change(){
 
 function multiply() {
     let userNum = document.querySelector("#num").value;
+    let userNum1 = document.querySelector("#num1").value;
     // console.log(userNum);
     let resultText = "";
-    for (let i = 1; i <= 10; i++) {
-      resultText += `${i} * ${userNum} = ${i * userNum} <br>`;
-    }
+   
+      resultText += `${userNum1} * ${userNum} = ${userNum1 * userNum} <br>`;
+    
     document.querySelector(".result").innerHTML = resultText;
   }
-  function multiplynextN() {
-    let user = document.querySelector("#num1").value;
-    // console.log(userNum);
-    let results = "";
-    for (let i = 1; i <= 10; i++) {
-
-
-      results += `${i} * ${userNum} = ${i * user} <br>`;
-    }
-    document.querySelector(".result1").innerHTML = results;
-  }
+ 
