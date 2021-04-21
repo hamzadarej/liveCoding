88d1962632e08e353809fae9178aeb33c24afabe
hamza <darejhamza@gmail.com>
2021-04-21 11:03:23 +0200

    js in browser

---
+++ april/21.04/src/script/index.js
@@ -0,0 +1,26 @@
+function change(){
+    console.log(window.document.getElementById('main'))
+    window.document.getElementById('main').outerHTML=
+    "<span>hi im from js </span>";
+}
+
+function multiply() {
+    let userNum = document.querySelector("#num").value;
+    // console.log(userNum);
+    let resultText = "";
+    for (let i = 1; i <= 10; i++) {
+      resultText += `${i} * ${userNum} = ${i * userNum} <br>`;
+    }
+    document.querySelector(".result").innerHTML = resultText;
+  }
+  function multiplynextN() {
+    let user = document.querySelector("#num1").value;
+    // console.log(userNum);
+    let results = "";
+    for (let i = 1; i <= 10; i++) {
+
+
+      results += `${i} * ${userNum} = ${i * userNum} <br>`;
+    }
+    document.querySelector(".result1").innerHTML = results;
+  }
