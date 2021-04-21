f6a053a89e288c839901764dd41a26e7c0b62cb9
hamza <darejhamza@gmail.com>
2021-04-21 12:23:25 +0200

    JS in browser

--- april/21.04/src/script/index.js
+++ april/21.04/src/script/index.js
@@ -4,14 +4,5 @@ function change(){
     "<span>hi im from js </span>";
 }
 
-function multiply() {
-    let userNum = document.querySelector("#num").value;
-    let userNum1 = document.querySelector("#num1").value;
-    // console.log(userNum);
-    let resultText = "";
-   
-      resultText += `${userNum1} * ${userNum} = ${userNum1 * userNum} <br>`;
-    
-    document.querySelector(".result").innerHTML = resultText;
-  }
+
  
