e7ea4f60bf4d0b90498c11a50a9eefae6a963c5d
hamza <darejhamza@gmail.com>
2021-04-27 16:42:20 +0200

    Dom quering

---
+++ april/27.04/index.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body");
+body.style.fontFamily="monospace";
+body.style.textAlign="Center";
+
+
+
+const header = document.querySelector("h1").style;
+header.padding="10px";
+const main =document.querySelector(".main").style;
+
+main.cssText = "display:flex; color:blue; text-shadow:1px 1px 1px black;  justify-content:space-between; align-items:center;"
