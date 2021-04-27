d14364a73e158b2aba61c170d00ba6d8fb8a7d07
hamza <darejhamza@gmail.com>
2021-04-27 16:53:09 +0200

    Dom quering

--- april/27.04/index.js
+++ april/27.04/index.js
@@ -9,3 +9,13 @@ header.padding="10px";
 const main =document.querySelector(".main").style;
 
 main.cssText = "display:flex; color:blue; text-shadow:1px 1px 1px black;  justify-content:space-between; align-items:center;"
+
+
+const textLI = document.querySelectorAll(".li");
+textLI.forEach(({style})=>{
+style.fontSize="1.5rem";
+style.fontStyle="bold";
+style.borderBottom="1px black solid";
+
+});
+
