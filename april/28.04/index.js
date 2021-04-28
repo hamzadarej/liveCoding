3bbdb933756429b6ba0f50fb4bf6eee5730e9c99
hamza <darejhamza@gmail.com>
2021-04-28 12:28:15 +0200

    Dom Traversing

--- april/28.04/index.js
+++ april/28.04/index.js
@@ -1,9 +1,9 @@
-const commentOne =document.querySelector("body").firstChild;
+const commentOne = document.querySelector("body").firstChild;
 console.log(commentOne.nextSibling);
 
 const commentTwo = document.querySelector("h1").nextSibling;
 console.log(commentTwo.nextSibling);
-const commentThree =document.querySelector("p").nextSibling;
+const commentThree = document.querySelector("p").nextSibling;
 console.log(commentThree.nextSibling);
 
 const commentFour = document.querySelector("span").previousSibling;
@@ -11,14 +11,15 @@ console.log(commentFour.previousSibling);
 
 const commentFive = document.querySelector("script").previousSibling;
 console.log(commentFive.previousSibling);
+
 /*easier method */
-const comments=document.querySelector("body");
-const commentsArr=Array.from(comments.childNodes);
 
-commentsArr.forEach(comment => {
-    if (comment.nodeType==("8"))
-    console.log(comment) 
-    
+const comments = document.querySelector("html").lastChild.childNodes;
+const commentsF = document.querySelector("html").firstChild.childNodes;
+
+const commentsArr = Array.from(comments);
+console.log(comments);
+
+commentsArr.forEach((comment) => {
+  if (comment.nodeType == "8") console.log(comment);
 });
-
-
