fa75547b181c7dedc343c5350958e32bb7117747
hamza <darejhamza@gmail.com>
2021-04-28 10:59:36 +0200

    Dom Traversing

--- april/28.04/index.js
+++ april/28.04/index.js
@@ -1,7 +1,15 @@
-const comments =document.querySelector("body").firstChild;
-console.log(comments.nextSibling);
+const commentOne =document.querySelector("body").firstChild;
+console.log(commentOne.nextSibling);
 
+const commentTwo = document.querySelector("h1").nextSibling;
+console.log(commentTwo.nextSibling);
+const commentThree =document.querySelector("p").nextSibling;
+console.log(commentThree.nextSibling);
 
+const commentFour = document.querySelector("span").previousSibling;
+console.log(commentFour.previousSibling)
 
+const commentFive = document.querySelector("script").previousSibling;
+console.log(commentFive.previousSibling)
 
 
