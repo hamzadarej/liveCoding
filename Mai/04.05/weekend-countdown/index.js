edfe935d96b8d1f8072fa808774d1401fa6d9966
hamza <darejhamza@gmail.com>
2021-05-04 14:19:04 +0200

    toLocalDate

--- Mai/04.05/weekend-countdown/index.js
+++ Mai/04.05/weekend-countdown/index.js
@@ -3,5 +3,9 @@ document.querySelector(".year").innerHTML=year;
 
 const checkNow = document.querySelector("#checkNow");
 document.querySelector("h1").innerHTML="hey, This is the weekend finder app";
-
-const getDayName
+const date = new Date();
+const getDayName = ()=> {
+    return date.toLocaleDateString("en-EN",{weekday:"long",});
+};
+console.log(getDayName());
+/*document.querySelector(".result").innerHTML=getDayName();*/
