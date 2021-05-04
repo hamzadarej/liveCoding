e363d70cc50a2c6b0045abff24e6546b118c4869
hamza <darejhamza@gmail.com>
2021-05-04 14:48:45 +0200

    toLocalDate

--- Mai/04.05/weekend-countdown/index.js
+++ Mai/04.05/weekend-countdown/index.js
@@ -5,7 +5,22 @@ const checkNow = document.querySelector("#checkNow");
 document.querySelector("h1").innerHTML="hey, This is the weekend finder app";
 const date = new Date();
 const getDayName = ()=> {
-    return date.toLocaleDateString("en-EN",{weekday:"long",});
+    return date.toLocaleDateString("fr-FR",{weekday:"long",});
 };
+const getTime =()=> {
+    return date.toLocaleDateString("fr-Fr")
+}
 console.log(getDayName());
 /*document.querySelector(".result").innerHTML=getDayName();*/
+
+function weekend(e) {
+    e.preventDefault();
+    const userName=document.querySelector("#userName").value;
+    const result =document.querySelector(".result");
+    if(userName !=""){
+        result.innerHTML=`hey ${userName},Today is ${getDayName()},${5-date.getDay()} days left till the weekend`;
+    }else{
+        document.querySelector("#userName").placeholder = "Do it now";
+    }
+}
+checkNow.addEventListener("click",weekend)
