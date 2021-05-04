0e55f5736343234bb8a9f962a1404ab0da5c0bd1
hamza <darejhamza@gmail.com>
2021-05-04 14:10:32 +0200

    toLocalDate

--- Mai/04.05/weekend-countdown/index.js
+++ Mai/04.05/weekend-countdown/index.js
@@ -1,1 +1,7 @@
-const get
+const year =new Date().getFullYear();
+document.querySelector(".year").innerHTML=year;
+
+const checkNow = document.querySelector("#checkNow");
+document.querySelector("h1").innerHTML="hey, This is the weekend finder app";
+
+const getDayName
