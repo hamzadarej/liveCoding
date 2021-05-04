d3ec7cea93fd4d9ea1d4812941804de2ebc7ad6f
hamza <darejhamza@gmail.com>
2021-05-04 14:59:07 +0200

    toLocalDate

--- Mai/04.05/weekend-countdown/index.js
+++ Mai/04.05/weekend-countdown/index.js
@@ -1,26 +1,31 @@
-const year =new Date().getFullYear();
-document.querySelector(".year").innerHTML=year;
+const year = new Date().getFullYear();
+document.querySelector(".year").innerHTML = year;
 
 const checkNow = document.querySelector("#checkNow");
-document.querySelector("h1").innerHTML="hey, This is the weekend finder app";
+document.querySelector("h1").innerHTML = "hey, This is the weekend finder app";
 const date = new Date();
-const getDayName = ()=> {
-    return date.toLocaleDateString("fr-FR",{weekday:"long",});
+const getDayName = () => {
+  return date.toLocaleDateString("fr-FR", { weekday: "long" });
 };
-const getTime =()=> {
-    return date.toLocaleDateString("fr-Fr")
-}
-console.log(getDayName());
+const getTime = () => {
+  return date.toLocaleDateString("fr-Fr", { day: "numeric" });
+};
+const getMonthName = () => {
+  return date.toLocaleDateString("fr-Fr", { month: "long" });
+};
+console.log(getMonthName());
 /*document.querySelector(".result").innerHTML=getDayName();*/
 
 function weekend(e) {
-    e.preventDefault();
-    const userName=document.querySelector("#userName").value;
-    const result =document.querySelector(".result");
-    if(userName !=""){
-        result.innerHTML=`hey ${userName},Today is ${getDayName()},${5-date.getDay()} days left till the weekend`;
-    }else{
-        document.querySelector("#userName").placeholder = "Do it now";
-    }
+  e.preventDefault();
+  const userName = document.querySelector("#userName").value;
+  const result = document.querySelector(".result");
+  if (userName != "") {
+    result.innerHTML = `hey ${userName},Today is ${getDayName()} the ${getTime()} ${getMonthName()},${
+      5 - date.getDay()
+    } days left till the weekend`;
+  } else {
+    document.querySelector("#userName").placeholder = "Do it now";
+  }
 }
-checkNow.addEventListener("click",weekend)
+checkNow.addEventListener("click", weekend);
