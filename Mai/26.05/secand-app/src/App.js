30624c7b288d3dc64f2348d483f3ec8513ea46c9
hamza <darejhamza@gmail.com>
2021-05-26 09:52:18 +0200

    import&export jsFile

--- Mai/26.05/secand-app/src/App.js
+++ Mai/26.05/secand-app/src/App.js
@@ -1,10 +1,12 @@
-import Text from "./components/Text"
+import {Text,Hallo} from "./components/Text"
+let name="hamza";
 function App() {
   return (
     <div className="App">
       <h1 className="App-header">
         hi im react
-       <Text/>
+       <Text />
+       <Hallo userName={name}/>
       </h1>
     </div>
   );
--- Mai/26.05/secand-app/src/components/Text.js
+++ Mai/26.05/secand-app/src/components/Text.js
@@ -1,4 +1,8 @@
+
 function Text(){
-    return <div> hey , I an components</div>
-}
-export default Text;
+    return (<div> hey , I an components</div>)
+};
+function Hallo(props){
+    return (<div>welcome {props.userName} to react </div>)
+};
+export  {Text,Hallo};
