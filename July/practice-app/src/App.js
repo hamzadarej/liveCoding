98e4363baa290b13eb2ffdc8448b0f1a53932b6b
hamza <darejhamza@gmail.com>
2021-07-05 13:50:03 +0200

    old-react-function

--- July/practice-app/src/App.js
+++ July/practice-app/src/App.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default class App extends React.Component {
-constructor(props){
-super(props);
-this.state = {
-data:{name:"hamza",age:28},
-isLogged:false,
-user:"hamza",
-}
-
-};
-
-render(){
-  return(
-  <div>my name is {this.state.data.name} and I'm {this.state.data.age} years old</div>
-
-
-  )}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: { name: "hamza", age: 28 },
+      isLogged: false,
+      user: "hamza",
+    };
+    this.sum = function (x, z) {
+      return x + z;
+    };
+    //this.handelSub=this.handelSub.bind(this)
+  }
+  add=()=>{console.log("im E6 function")}
+  //function have to be bind
+  handelSub() {
+    console.log("im a normal function");
+  }
+  render() {
+    return (
+      <div>
+        my name is {this.state.data.name} and I'm {this.state.data.age} years
+        old
+        {this.sum(10, 8)}
+        {this.handelSub()}
+        {this.add()}
+      </div>
+    );
+  }
 }
