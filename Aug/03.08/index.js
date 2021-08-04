2b3aeadec43e13ee2ac73fc2314291861e1c0e08
hamza <darejhamza@gmail.com>
2021-08-04 09:00:44 +0200

    file sys

--- Aug/03.08/index.js
+++ Aug/03.08/index.js
@@ -3,24 +3,22 @@
 const fs = require("fs");
 // create file
 // writeFile will create or overwrite
-/*
-// appendfile will create file
+
+// appendfile will create file => non blocking 
 fs.appendFile("newText.txt","I am text from nodejs 🥳 \n",(err)=>{if(err) throw err ;
-console.log("File is done 👍 line 7 ")});
+console.log("File is done 👍 line 9 ")});
 
-
-// read File
+// read File => non blocking code (asynchronous )
 fs.readFile("./longText.txt","utf8",(err,data)=>{
     if (err) throw err;
-    console.log(data , "line 13");
+    console.log(data , "line 14");
 });
-
 // read file Sync => blocking code
-const data = fs.readFileSync("./longText.txt");
-console.log(data.toString(),"line 18");
-console.log("reading file is done ✅ line 19")
+//const data = fs.readFileSync("./longText.txt"); // blocks here until file is read
+//console.log(data.toString(),"line 18");
+//console.log("reading file is done ✅ line 19")
 // rename file 
-fs.rename("./hello.txt","./hey.text",(err)=>{
+/*fs.rename("./hello.txt","./hey.text",(err)=>{
     if(err)throw err;
     console.log("Rename file done ✅ ");
 })
@@ -30,17 +28,19 @@ fs.unlink(filePathName, (err)=>{
     if (err) throw err;
     console.log("File deleted , ❎")
 })
-
+*/
 // copy file 
 fs.copyFile("./original.txt","./copyFromOG.txt",(err)=>{
     if(err)throw err;
-    console.log("copy file,Done ☑️ line 24")
+    console.log("copy file,Done ☑️ line 35")
 })
 
-*/
+
 
 fs.open("./highText.txt", "r", (err, fd) => {
   if (err) throw err;
   console.log(fd);
   console.log("flags ");
 });
+
+
