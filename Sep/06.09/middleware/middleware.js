dbd913845255f56f31022036fc7f6c4f8b6b8893
hamza <darejhamza@gmail.com>
2021-09-08 12:41:14 +0200

    check exicting user ,middleware

--- Sep/06.09/middleware/middleware.js
+++ Sep/06.09/middleware/middleware.js
@@ -1,17 +1,15 @@
 const UsersModel = require("../model/usersModel");
-var  {validationResult } = require("express-validator");
+
 const userExict = async (req, res, next) => {
   const userByEmail = await UsersModel.findOne({ email: req.body.email });
   const userEmail = req.body.email;
-const errors = validationResult(req);
+
 if (userByEmail?.email == userEmail) {
     return res.render("index",{
         title: "you are already registered",
-        errors:errors.array()
-    });
+        });
   }
-
-  next();
+next();
 };
 
 module.exports = userExict;
