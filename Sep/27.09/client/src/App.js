33aa5bcde28bd9e643eedb92ddf798b83f5e7627
hamza <darejhamza@gmail.com>
2021-09-27 11:20:45 +0200

    full stack app

--- Sep/27.09/client/src/App.js
+++ Sep/27.09/client/src/App.js
@@ -1,38 +1,33 @@
-import {useEffect,useState} from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-  const [users,setUsers]= useState([]);
-  const getData =()=>{
+  const [users, setUsers] = useState([]);
+  const getData = () => {
     fetch("/users")
-    .then((results)=>results.json())
-    .then((data)=>setUsers(data))
-  }
+      .then((results) => results.json())
+      .then((data) => setUsers(data));
+  };
   useEffect(() => {
-    getData()
-  },[]);
-  const usersList =users.map(({_id,username,role,avatar})=>
-  
-  <table>
-  <tr key={_id}>
-    <th>username</th>
-    <th>role</th>
-    <th>image</th>
-  </tr>
-  <tr>
-    <td>{username}</td>
-    <td>{role}</td>
-    
-      <td><img src={avatar} alt={username} /></td>
-   
-  </tr>
+    getData();
+  }, []);
+  const usersList = users.map(({ _id, username, role, avatar }) => (
+    <table >
+      <tr key={_id}>
+        <th>username</th>
+        <th>role</th>
+        <th>image</th>
+      </tr>
+      <tr>
+        <td>{username}</td>
+        <td>{role}</td>
 
-</table>
-  )
-  return (
-    <div className="App">
-      {usersList}
-    </div>
-  );
+        <td>
+          <img src={avatar} alt={username} />
+        </td>
+      </tr>
+    </table>
+  ));
+  return <div className="App">{usersList}</div>;
 }
 
 export default App;
